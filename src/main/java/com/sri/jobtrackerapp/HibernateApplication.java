ffb7d824e98d549957a18821eb38203b0ab77eae
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-08-08 14:18:05 -0700

    add site

--- src/main/java/com/sri/jobtrackerapp/HibernateApplication.java
+++ src/main/java/com/sri/jobtrackerapp/HibernateApplication.java
@@ -1,16 +1,15 @@
-package com.sri.jobtrackerapp ;
+package com.sri.jobtrackerapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-//@CrossOrigin(origins = "http://localhost:3000") 
-@CrossOrigin(origins = {"http://localhost:3000", "https://jobtrackerfe.netlify.app"})
+@CrossOrigin(origins = {"http://localhost:3000", "https://jobtrackerfe.netlify.app/", "jobtracker.up.railway.app"})
 public class HibernateApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HibernateApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateApplication.class, args);
+    }
 
 }
