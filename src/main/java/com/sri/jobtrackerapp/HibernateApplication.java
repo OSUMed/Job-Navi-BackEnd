56ac6e802cb217ceb62523d6c08053a97f3675c4
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-08-08 14:56:27 -0700

    CORS proxy add

--- src/main/java/com/sri/jobtrackerapp/HibernateApplication.java
+++ src/main/java/com/sri/jobtrackerapp/HibernateApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @SpringBootApplication
 @CrossOrigin(
-	    origins = {"http://localhost:3000", "https://jobtrackerfe.netlify.app/", "https://jobtrackerfe.netlify.app/", "https://jobtrackerbackend.up.railway.app"},
+	    origins = {"http://localhost:3000", "https://cors-anywhere-osu.up.railway.app", "https://jobtrackerfe.netlify.app/", "https://jobtrackerfe.netlify.app/", "https://jobtrackerbackend.up.railway.app"},
 	    allowedHeaders = {"Authorization", "Content-Type"}, // Add the headers you want to allow
 	    methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE} // Add the HTTP methods you want to allow
 	)
